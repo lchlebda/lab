713283f4a01a7ea838ecb8419f361e3b0e4b13d8
Mysz <achlebda@gmail.com>
2016-05-02 11:30:22 +0200

    CODILITY - Binary Gap

---
+++ src/main/java/codility/BinaryGap.java
@@ -0,0 +1,37 @@
+package codility;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * https://codility.com/programmers/task/binary_gap/
+ */
+public class BinaryGap {
+
+    private static final Logger logger = LogManager.getLogger(BinaryGap.class);
+
+    public static void main(String[] args) {
+        logger.info("Result: {} ", binaryGap(7));
+    }
+
+    private static int binaryGap(int N) {
+        int max = 0;
+        int counter = 0;
+        boolean isGapStarted = false;
+        do {
+            int r = N % 2;
+            N = (N-r) / 2;
+            if (r == 1) {
+                if (counter > max) {
+                    max = counter;
+                }
+                isGapStarted = true;
+                counter = 0;
+            } else if (isGapStarted) {
+                counter++;
+            }
+        } while (N > 0);
+
+        return max;
+    }
+}

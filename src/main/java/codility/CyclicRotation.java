757a67006b992704ad59628526718f868014bafb
Mysz <achlebda@gmail.com>
2016-05-02 21:21:17 +0200

    CODILITY - Cyclic Rotation, Odd occurrences in array

---
+++ src/main/java/codility/CyclicRotation.java
@@ -0,0 +1,43 @@
+package codility;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * https://codility.com/programmers/task/cyclic_rotation/
+ */
+public class CyclicRotation {
+
+    private static final Logger logger = LogManager.getLogger(CyclicRotation.class);
+
+    public static void main(String[] args) {
+        logger.info("Result: {} ", cyclicRotation(new int[]{3}, 0));
+    }
+
+    private static int[] cyclicRotation(int[] A, int K) {
+        int N = A.length;
+        if (N == 0 || K == 0) {
+            return A;
+        }
+        if (N > 100) {
+            throw new IllegalArgumentException("Wrong size of array provided!");
+        }
+        if (K < 0 || K > 100) {
+            throw new IllegalArgumentException("Number of rotations doesn't fit the set [0..100]!");
+        }
+        for (int a : A) {
+            if (a > 1000 || a < -1000) {
+                throw new IllegalArgumentException("At least one number in array doesn't fit the set [-1000..1000]!");
+            }
+        }
+
+        int[] result = new int[N];
+        for (int i=0; i<K; i++) {
+            result[0] = A[N-1];
+            System.arraycopy(A, 0, result, 1, N - 1);
+            System.arraycopy(result, 0, A, 0, N);
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/codility/OddOccurrencesInArray.java
@@ -0,0 +1,34 @@
+package codility;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * https://codility.com/programmers/task/odd_occurrences_in_array/
+ */
+public class OddOccurrencesInArray {
+
+    private static final Logger logger = LogManager.getLogger(OddOccurrencesInArray.class);
+
+    public static void main(String[] args) {
+        logger.info("Result: {} ", solution(new int[]{9, 9, 3, 1, 5, 4, 4, 3, 5}));
+    }
+
+    private static int solution(int[] A) {
+        Map<Integer, Boolean> map = new HashMap<>();
+        for (int a : A) {
+            if (map.get(a) != null) {
+                map.remove(a);
+            } else {
+                map.put(a, true);
+            }
+        }
+
+        Set<Map.Entry<Integer, Boolean>> set =  map.entrySet();
+        return set.iterator().next().getKey();
+    }
+}

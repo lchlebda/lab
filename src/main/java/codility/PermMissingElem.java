2bd0f3295663501b85ea2164737ebadec1c8b95c
Mysz <achlebda@gmail.com>
2016-05-02 23:39:22 +0200

    CODILITY - Perm missing elem

---
+++ src/main/java/codility/PermMissingElem.java
@@ -0,0 +1,29 @@
+package codility;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * https://codility.com/programmers/task/perm_missing_elem/
+ */
+public class PermMissingElem {
+
+    private static final Logger logger = LogManager.getLogger(PermMissingElem.class);
+
+    public static void main(String[] args) {
+        int[] arr = new int[200111];
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = i+2;
+        }
+        logger.info("Result: {} ", solution(arr));
+    }
+
+    private static int solution(int[] A) {
+        long sum = 0;
+        for (int a : A) {
+            sum += a;
+        }
+
+        return (int)((long)(2 + A.length)*(A.length+1)/2 - sum);
+    }
+}
